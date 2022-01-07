5ff3412a55e938922a3c5d02a56985f66ace7051
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2022-01-07 09:32:26 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,11 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  video = createCapture(VIDEO);
+  video.size(600, 300);
+  
+  poseNet = ml5.poseNet( video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
 
@@ -161,4 +166,9 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+
+
+function modelLoaded() {
+  console.log("Model Is Loaded");
 }
